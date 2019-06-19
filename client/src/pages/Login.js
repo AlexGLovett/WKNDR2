0476cc80f19d80e0b38a5efc2e6e9f8c93acd013
AlexGLovett <alexanderglovett@gmail.com>
2019-06-19 00:32:17 -0400

    initial user setup and backend

---
+++ client/src/pages/Login.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+//import API from "../utils/API";
+//import { Link } from "react-router-dom";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Wkndr</h1>
+        <li>
+          Sign In
+          {/* <Link to="/urwknd">Sign In</Link> */}
+        </li>
+        <li>
+          Sign Up
+          {/* <Link to="/search">Sign Up</Link> */}
+        </li>
+      </div>
+    );
+  }
+}
+
+export default Login;
